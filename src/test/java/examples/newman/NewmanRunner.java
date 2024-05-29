d475ce37d07eeab39a48c1fa3c78222eb4bc56e1
Liem Nguyen <nliem@hitachivantara.com>
2024-05-29 11:39:29 +0700

    commit 6

--- src/test/java/examples/newman/NewmanRunner.java
+++ src/test/java/examples/newman/NewmanRunner.java
@@ -14,16 +14,22 @@ import com.opencsv.exceptions.CsvException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+
 class NewmanRunner {
     
     @Karate.Test
     Karate testTodos() {
-        getDataFromDB();
+        getDataFromDBAndWriteToCSVFile();
         // String cmd = getCommandWithData();
         executeCommand();
         return Karate.run("callNewman").relativeTo(getClass());
     } 
-    public void getDataFromDB(){
+    public void getDataFromDBAndWriteToCSVFile(){
          try {
             Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//172.18.14.33:1521/orcl2", "hr", "hr");
 
@@ -41,63 +47,74 @@ class NewmanRunner {
                 System.out.println("User age: " + age);
                 csvWriter.append(username + "," + age +  "," + skill + "," + userid + "\n");
             }
+
             csvWriter.flush();
             csvWriter.close();
 
             System.out.println("Record to file successfully");
+            System.out.println();
+            
         } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
     }
-    // public String getCommandWithData(){
-    //     String csvFile = "D://output.csv";
-    //     String csvFilePathCMd = "$(type D://output.csv)";
-    //     try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
-    //         List<String[]> rows = reader.readAll();
-    //         StringBuilder csvData = new StringBuilder();
-    //         for (String[] row : rows) {
-    //             for (String cell : row) {
-    //                 csvData.append(cell).append(",");
-    //             }
-    //             csvData.deleteCharAt(csvData.length() - 1); // Remove the last comma
-    //             csvData.append("\n");
-    //         }
-    //         System.out.println("CSV Data:");
-    //         System.out.println(csvData.toString());
-
-    //         String command = "cmd.exe /c newman run https://api.postman.com/collections/15895208-71136bfa-1d7a-48f1-b825-a5d4ba7531b9?access_key=PMAT-01HYDDJYES08EAMP4J2MC3Y32F -e ./mockapi_env.postman_environment.json --global-var csv-data=\"" + csvFilePathCMd + "\""+" --reporters junit --reporter-junit-export ./target/newman-reports/report.xml";
-           
-    //         System.out.println(command);
-
-    //         return command;
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     } catch (CsvException e) {
-    //         e.printStackTrace();
-    //     }
-    //     return null;
-    // }
-    public void executeCommand(){
-         try {
-            String csvFilePath = "D:\\output.csv";
-            StringBuilder csvContent = new StringBuilder();
-            try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    csvContent.append(line).append("\n");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+    public String getDataFromCSVFile(String csvFilePath){
+        StringBuilder csvContent = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                csvContent.append(line).append("\n");
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        String csvData = csvContent.toString().replace("\"", "\\\"");
+        return csvData;
+    }
+    
+    public void executeCommand(){
+        String jsonFilePath = "./src/test/java/examples/newman/collections.json";
+        try (FileReader reader = new FileReader(jsonFilePath)) {
+            Gson gson = new Gson();
+            JsonArray jsonArray = JsonParser.parseReader(reader).getAsJsonArray();
 
-            String csvData = csvContent.toString().replace("\"", "\\\"");
-            String command = "cmd.exe /c newman run https://api.postman.com/collections/15895208-71136bfa-1d7a-48f1-b825-a5d4ba7531b9?access_key=PMAT-01HYDDJYES08EAMP4J2MC3Y32F -e ./mockapi_env.postman_environment.json --global-var csv-data=\"" + csvData + "\" --reporters junit --reporter-junit-export ./target/newman-reports/report.xml";
-            System.out.println(command);
-            Process process = java.lang.Runtime.getRuntime().exec(command);
-            process.waitFor();
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
+                String collection = jsonObject.get("collection").getAsString();
+                String environment = jsonObject.get("environment").getAsString();
+                String csvDataFilePath = jsonObject.get("csvDataFilePath").getAsString();
+                String reportName = jsonObject.get("reportName").getAsString();
+                // int iteration = jsonObject.get("iteration").getAsInt();
 
+                System.out.println();
+                System.out.println("Object " + (i + 1) + ":");
+                System.out.println("Collection: " + collection);
+                System.out.println("Environment: " + environment);
+                System.out.println("CSV File Path: " + csvDataFilePath);
+                System.out.println();
+
+                String csvData = getDataFromCSVFile(csvDataFilePath);
+                String command = "cmd.exe /c newman run \"" + collection + "\" -e \"" + environment + "\" --global-var csv-data=\"" + csvData + "\" --reporters junit --reporter-junit-export ./target/newman-reports/\"" + reportName + "\".xml";
+            
+                System.out.println(command);
+                Process process = java.lang.Runtime.getRuntime().exec(command);
+                process.waitFor();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        //  try {
+        //     String csvData = getDataFromCSVFile("D:\\output.csv");
+        //     String command = "cmd.exe /c newman run https://api.postman.com/collections/15895208-71136bfa-1d7a-48f1-b825-a5d4ba7531b9?access_key=PMAT-01HYDDJYES08EAMP4J2MC3Y32F -e ./mockapi_env.postman_environment.json --global-var csv-data=\"" + csvData + "\" --reporters junit --reporter-junit-export ./target/newman-reports/report.xml";
+            
+        //     System.out.println(command);
+        //     Process process = java.lang.Runtime.getRuntime().exec(command);
+        //     process.waitFor();
+
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
     }   
 }
